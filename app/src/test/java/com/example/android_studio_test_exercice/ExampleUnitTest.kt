1d7bda694d1be450f388e934ce0743c16dd6c589
MarionaArenillas <mariona.arenillas@gracia.lasalle.cat>
2026-04-28 16:40:36 +0200

    Crea estructura inicial de Unit Testing

--- app/src/test/java/com/example/android_studio_test_exercice/ExampleUnitTest.kt
+++ app/src/test/java/com/example/android_studio_test_exercice/ExampleUnitTest.kt
@@ -1,17 +1,36 @@
 package com.example.android_studio_test_exercice
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.compose.ui.state.ToggleableState
+import com.example.android_studio_test_exercice.viewmodel.MainViewModel
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
-import org.junit.Assert.*
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
+
+    // Fa que LiveData funcioni de forma síncrona durant els tests
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    // ViewModel que testejarem
+    private lateinit var viewModel: MainViewModel
+
+    // S'executa abans de cada test
+    @Before
+    fun setup() {
+        viewModel = MainViewModel()
+    }
+
+    // Comprova els valors inicials del ViewModel
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun initialState() {
+        assertEquals(true, viewModel.estatSwitch.value)
+        assertEquals(false, viewModel.esVegetaria.value)
+        assertEquals(false, viewModel.esVega.value)
+        assertEquals(true, viewModel.esCarnivor.value)
+        assertEquals(ToggleableState.Off, viewModel.triStateStatus.value)
+        assertEquals("Messi", viewModel.selectedOption.value)
     }
 }
